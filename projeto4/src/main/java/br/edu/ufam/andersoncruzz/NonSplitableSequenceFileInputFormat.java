01d8d6a33fdc8fd0e88ac1fa77a31e49ef129449
Anderson Cruz <ac.andersoncruz@gmail.com>
2017-06-23 05:28:49 -0400

    pagerank

---
+++ projeto4/src/main/java/br/edu/ufam/andersoncruzz/NonSplitableSequenceFileInputFormat.java
@@ -0,0 +1,12 @@
+package br.edu.ufam.andersoncruzz;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.mapreduce.JobContext;
+import org.apache.hadoop.mapreduce.lib.input.SequenceFileInputFormat;
+
+public class NonSplitableSequenceFileInputFormat <K, V> extends SequenceFileInputFormat <K, V>{
+
+	protected boolean isSplitable(JobContext context, Path filename){
+		return false;
+	}
+}
